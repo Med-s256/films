f56aaab2e80323ad69262a684e62f3cf5c0ad2d5
med skib <skib.med@gmail.com>
2023-02-08 17:37:45 +0100

    card style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,46 @@ import { Film } from './models/film.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  firstFilm!: Film;
-  secondFilm!: Film;
-  thirdFilm!: Film;
+  films!: Film[];
   ngOnInit(): void {
-    this.firstFilm = new Film(
+    this.films = [new Film(
       'Titanic',
       'romantic film',
       new Date(),
       'https://upload.wikimedia.org/wikipedia/commons/9/9c/Titanic_wreck_bow.jpg',
       5
-    );
-    this.secondFilm = new Film(
+    ),
+    new Film(
       'Mission impossible',
       'action film',
       new Date(),
       'https://occ-0-114-33.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABeqXBbDRJ-bll6rdRKEO0HTmu0cOIJH7m3VeB9yi6eCqKDoxqpnsKopu-_XNgeB2uck7swIkYVKAQtv5Sdw3JJAs5dIpNo0_zosX.jpg?r=93f',
       0,
       "Frensh film"
-    );
-    this.thirdFilm= new Film(
+    ),
+    new Film(
       "Bakhta",
       "music",
       new Date(),
       'https://i.ytimg.com/vi/O6zEDqvxGXE/maxresdefault.jpg',
-      2,
+      150,
       "Algerian music"
+    ),
+    new Film(
+      'Titanic',
+      'romantic film',
+      new Date(),
+      'https://upload.wikimedia.org/wikipedia/commons/9/9c/Titanic_wreck_bow.jpg',
+      5
+    ),
+    new Film(
+      'Mission impossible',
+      'action film',
+      new Date(),
+      'https://occ-0-114-33.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABeqXBbDRJ-bll6rdRKEO0HTmu0cOIJH7m3VeB9yi6eCqKDoxqpnsKopu-_XNgeB2uck7swIkYVKAQtv5Sdw3JJAs5dIpNo0_zosX.jpg?r=93f',
+      255,
+      "Frensh film"
     )
-  }
+  ]
+  } 
 }
